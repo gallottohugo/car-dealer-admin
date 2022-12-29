9f181e666acf0bed7fc828e41ed41c8323ae4f11
Hugo Gallotto <gallottohugo@gmail.com>
2022-12-29 14:29:48 -0300

    First commit

---
+++ app.ts
@@ -0,0 +1,63 @@
+import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+import express from 'express'
+import Connect from 'connect-pg-simple'
+import session from 'express-session'
+
+const PORT = 3000
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+}
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN)
+  }
+  return null
+}
+
+const start = async () => {
+  const app = express()
+
+  const admin = new AdminJS({})
+
+  const ConnectSession = Connect(session)
+  const sessionStore = new ConnectSession({
+    conObject: {
+      connectionString: 'postgres://postgres:@localhost:5432/car_dealer',
+      ssl: process.env.NODE_ENV === 'production',
+    },
+    tableName: 'session',
+    createTableIfMissing: true,
+  })
+
+  const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookieName: 'adminjs',
+      cookiePassword: 'sessionsecret',
+    },
+    null,
+    {
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: true,
+      secret: 'sessionsecret',
+      cookie: {
+        httpOnly: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production',
+      },
+      name: 'adminjs',
+    }
+  )
+  app.use(admin.options.rootPath, adminRouter)
+
+  app.listen(PORT, () => {
+    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+  })
+}
+
+start()
